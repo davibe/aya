6f30644731d465b955df1da0d1548ee31f3a2041
Davide Bertola <dade@dadeb.it>
2022-06-05 15:34:13 +0200

    add aya-gen integration test

---
+++ test/cases/030_aya-gen/030_unload/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# SUMMARY: Check that aya-gen works
+# LABELS:
+
+set -ex
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+# Test code goes here
+
+./target/debug/aya-gen generate tcphdr
+find /usr/src -name tcp.h
+
